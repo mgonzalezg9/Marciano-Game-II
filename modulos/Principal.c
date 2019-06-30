f96f2a195143c6ded05dab12fbce9eb0e8cc5415
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-06-30 12:50:27 +0200

    Rediseño de personaje y algunos dialogos
    Corregidos cambios menores.
    Tamaño del personaje y algunos dialogos adaptados al tamaño de la pantalla.

--- modulos/Principal.c
+++ modulos/Principal.c
@@ -7,17 +7,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+
 #define PuntosBalazo 20
-#define WPersonaje 64
-#define HPersonaje 64
+//#define WPersonaje 64
+//#define HPersonaje 64
 #define WEnemigo 80
 #define HEnemigo 80
 #define WBala 15
 #define HBala 45
 #define WVida 25
 #define HVida 25
-#define VxPersonaje 20
-#define VyPersonaje 20
+//#define VxPersonaje 20
+//#define VyPersonaje 20
 #define VxEnemigo 10
 #define VyEnemigo 10
 #define VyBala 30
@@ -42,12 +43,21 @@ int main(int argc, char **argv)
 {
     // Cargado de las imágenes y creación de la pantalla
     Imagen fondo1, fondo2, fondo3, fondo4;
-    Pantalla_Crea("Marciano Game II 1.4",1280,960);
+    Pantalla_Crea("Marciano Game II", 1280, 960);
+
     int wPantalla = Pantalla_Anchura();
     int hPantalla = Pantalla_Altura();
+
+    // Las caracteristicas del personaje van en funcion de la pantalla
+    int WPersonaje = wPantalla/12;
+    int HPersonaje = WPersonaje;
+
+    int VPersonaje = WPersonaje/4;
+
     Pantalla_ColorRelleno(255,255,255,255);
     Pantalla_ColorTrazo(0,255,0,255);
     int fin = 0;
+
     // Lectura de imágenes
     fondo1=Pantalla_ImagenLee("bg/PantallaInicio.bmp",0);
     fondo2=Pantalla_ImagenLee("bg/EleccionPersonaje.bmp",0);
@@ -58,68 +68,98 @@ int main(int argc, char **argv)
     Imagen Im3=Pantalla_ImagenLee("sprites/estrella.bmp",1);
     Imagen Im4=Pantalla_ImagenLee("sprites/bala.bmp",1);
     Imagen Im5=Pantalla_ImagenLee("sprites/vida.bmp",1);
-    // Contador de interaciones
-    int fps = 0;
+
+    // Indicador de primera vez
+    int flag = 1;
+    double ti;
     // FASE 1
     printf("[Fase 1]\n");
-    while(Pantalla_Activa()&&(!fin))
-    {
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RETURN))
-        {
+    while(Pantalla_Activa() && !fin) {
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RETURN)) {
             fin=1;
         }
-        if (fps>30) fps=0; // Reinicia el contador para evitar un posible desbordamiento
-        fps++;
         Pantalla_DibujaImagen(fondo1,0,0,wPantalla,hPantalla);
-        //Pantalla_DibujaTexto("Saludos Jugador",300,200);
-        if (fps%30<=15) Pantalla_DibujaTexto("Pulsa la tecla [Enter] para continuar",10,hPantalla-35);
+
+        if (flag || time(NULL) - ti < 1) { // Si es la primera vez o no ha pasado un segundo dibujar texto
+            Pantalla_DibujaTexto("Pulsa la tecla [Enter] para continuar",10,hPantalla-35);
+        } else {
+            if (time(NULL) - ti > 1.2) { // Si no se ha dibujado el texto desde hace 1.2 - 1 seg reiniciar el tiempo
+                ti = time(NULL);
+            }
+        }
+
+        if (flag) flag = 0; // Indica que ya no es la primera vez
+
         Pantalla_Actualiza();
         Pantalla_Espera(40);
     }
+
     fin = 0;
     // Liberado de elementos de la pantalla anterior
     Pantalla_ImagenLibera(fondo1);
+
     // Imágenes del personaje a elegir
     Imagen Imp1=Pantalla_ImagenLee("sprites/personaje.bmp",1);
     Imagen Imp2=Pantalla_ImagenLee("sprites/personaje2.bmp",1);
     Imagen Imp3=Pantalla_ImagenLee("sprites/personaje3.bmp",1);
+
     // Variables de control
-    int sel=0;
-    int flag=1;
+    int sel=0; // Indica la skin a escoger
+    flag=1; // Indica que se acaba de pulsar el boton
+
     // FASE 2
     printf("[Fase 2]\n");
-    while(Pantalla_Activa()&&(!fin))
-    {
+    while(Pantalla_Activa()&&(!fin)) {
         // Condición de cambio de fase
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_SPACE))
-        {
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_SPACE)) {
             fin=1;
         }
+
         // Elección de la imagen que tendrá el personaje
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT)&&(flag==1)){sel++; flag=0;}
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT)&&(flag==1)) {sel--; flag=0;}
-        if (!Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT) && !Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT)) flag=1;
-        switch (sel)
-        {
-            case 0: Im=Imp1; break;
-            case 1: Im=Imp2; break;
-            case 2: Im=Imp3; break;
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT) && flag) {
+            sel++; 
+            flag=0;
         }
-        if (sel>2) sel=2; // Evita que se salga del rango de imágenes
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT) && flag) {
+            sel--;
+            flag=0;
+        }
+        if (!Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT) && !Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT)) // Si no se pulsa ningún botón puede elegirse de nuevo
+            flag=1;
+
+        // Según lo elegido se imprime una u otra imágen
+        switch (sel) {
+            case 0: 
+                Im=Imp1;
+                break;
+            case 1: 
+                Im=Imp2;
+                break;
+            case 2:
+                Im=Imp3;
+                break;
+        }
+
+        // Evita que se salga del rango de imágenes
+        if (sel>2) sel=2;
         if (sel<0) sel=0;
+
         // Dibujado de fondos y menús
-        Pantalla_DibujaImagen(fondo2,0,0,wPantalla,hPantalla);
-        Pantalla_DibujaImagen(Im,wPantalla/2-WPersonaje,hPantalla/2+50,WPersonaje*2,HPersonaje*2);
-        Pantalla_DibujaTexto("Gracias por pulsar la tecla.",100,170);
-        Pantalla_DibujaTexto("Para seleccionar un personaje usa las flechas.",100,200);
-        Pantalla_DibujaTexto("Para jugar pulse [Espacio].",100,230);
+        Pantalla_DibujaImagen(fondo2, 0, 0, wPantalla, hPantalla);
+        Pantalla_DibujaImagen(Im, wPantalla/2-WPersonaje, 0.65 * hPantalla, WPersonaje*2, HPersonaje*2); // Se dibuja la imagen a elegir el doble de grande
+        Pantalla_DibujaTexto("Gracias por pulsar la tecla.",wPantalla/3, hPantalla/4);
+        Pantalla_DibujaTexto("Para seleccionar un personaje usa las flechas.", wPantalla/3, hPantalla * 0.3);
+        Pantalla_DibujaTexto("Para jugar pulse [Espacio].", wPantalla/3, hPantalla * 0.35);
+
         // Refresco de la pantalla
         Pantalla_Actualiza();
         Pantalla_Espera(40);
     }
+
     srand(time(NULL));
     // Liberado del fondo
     Pantalla_ImagenLibera(fondo2);
+
     // Inicialización de TDA
     Personaje p=PersonajeCrea(Im,WPersonaje,HPersonaje,rand()%(wPantalla-WPersonaje),hPantalla-HPersonaje);
     Enemigos e1=EnemigosCrea();
@@ -127,9 +167,10 @@ int main(int argc, char **argv)
     Balas b=BalasCrea();
     Vidas v=VidasCrea(VidaInicial);
     VidasRellena(v,Im5,WVida,HVida,DesplzVidas,PosVidas);
+
     // Preparación del nivel
     fin=0;
-    int ti=time(NULL);
+    ti=time(NULL);
     int t2;
     int puntos;
     int puntos_estrellas=0;
@@ -139,24 +180,25 @@ int main(int argc, char **argv)
     int BPuntos=0;
     flag=1;
     int primvez=1; // Determina si se ha disparado ya antes
+
     // FASE 3
     printf("[Fase 3]\n");
-    while(Pantalla_Activa()&&(!fin)&&(VidasGetVida(v)>0)/*&&(puntos>=0)*/)
-    {
+    while(Pantalla_Activa() && !fin && VidasGetVida(v)>0) {
         // Condición de final de juego
         if (Pantalla_TeclaPulsada(SDL_SCANCODE_TAB))
         {
             fin=1;
         }
+
         // Recuento de los puntos
         t2=time(NULL);
-        //if (puntos<0) puntos=t2-ti+puntos_estrellas+BPuntos;
-        puntos=t2-ti+puntos_estrellas-puntos_enemigo+BPuntos;
+        puntos= t2-ti + puntos_estrellas - puntos_enemigo + BPuntos;
+
         // Aparición de objetos
         if (rand()%ProbApEstrellas==1)
         {
-            Estrella e1=EstrellaCrea(Im3,32,32,rand()%(wPantalla-WPersonaje),rand()%(hPantalla-HPersonaje));
-            EstrellasInsertaNuevaEstrella(e,e1);
+            Estrella e1=EstrellaCrea(Im3, WPersonaje/3, HPersonaje/3, rand()%(wPantalla-WPersonaje), rand()%(hPantalla-HPersonaje));
+            EstrellasInsertaNuevaEstrella(e, e1);
         }
         if (rand()%ProbApEnemigos<2) // 20% de probabilidad de que aparezca un enemigo
         {
@@ -175,19 +217,21 @@ int main(int argc, char **argv)
             printf("[Vidas] Restantes: %d\n", VidasGetVida(v));
         }
         BPuntos+=BalasColision(b,e1)*PuntosBalazo;
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_F)&&(flag==1)){
-                Bala bala=BalaCrea(Im4,WBala,HBala,PersonajeGetX(p)+25,PersonajeGetY(p),VyBala);
-                BalasInsertaNuevaBala(b, bala);
-                flag=0;
-                primvez=0;
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_F) && flag) {
+            Bala bala=BalaCrea(Im4,WBala,HBala,PersonajeGetX(p)+25,PersonajeGetY(p),VyBala);
+            BalasInsertaNuevaBala(b, bala);
+            flag=0;
+            primvez=0;
         }
         if (!Pantalla_TeclaPulsada(SDL_SCANCODE_F)) flag=1;
         sprintf(mensaje,"Puntos = %d", puntos);
+
         // Movimiento del personaje
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT)) PersonajeMueve(p,-VxPersonaje,0);
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT)) PersonajeMueve(p,VxPersonaje,0);
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_UP)) PersonajeMueve(p,0,-VxPersonaje);
-        if (Pantalla_TeclaPulsada(SDL_SCANCODE_DOWN)) PersonajeMueve(p,0,VxPersonaje);
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_LEFT)) PersonajeMueve(p,-VPersonaje,0);
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_RIGHT)) PersonajeMueve(p,VPersonaje,0);
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_UP)) PersonajeMueve(p,0,-VPersonaje);
+        if (Pantalla_TeclaPulsada(SDL_SCANCODE_DOWN)) PersonajeMueve(p,0,VPersonaje);
+
         // Dibuja todo lo que hay en la pantalla
         Pantalla_DibujaImagen(fondo3,0,0,wPantalla,hPantalla);
         EstrellasDibuja(e);
@@ -196,11 +240,13 @@ int main(int argc, char **argv)
         BalasDibuja(b);
         EnemigosDibuja(e1);
         PersonajeDibuja(p);
+
         // Dibuja un pequeño menú
         Pantalla_DibujaTexto("Para terminar el juego pulse [Tab].",10,10);
         Pantalla_DibujaTexto(mensaje,10,40);
         VidasDibuja(v);
         if (primvez) Pantalla_DibujaTexto("Pulse [f] para disparar",10,hPantalla-35); // En el caso de que no se haya disparado antes muestra el control
+        
         // Refresco de la pantalla
         Pantalla_Actualiza();
         Pantalla_Espera(40);
@@ -221,6 +267,7 @@ int main(int argc, char **argv)
     Pantalla_ImagenLibera(Imp1);
     Pantalla_ImagenLibera(Imp2);
     Pantalla_ImagenLibera(Imp3);
+
     // Guardado de la puntuación
     printf("[Guardado] Puntuacion\n");
     int datoleido;
