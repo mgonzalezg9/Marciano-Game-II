451a5f83e31714365d9aa83ffe4bf69f609b6873
Manuel Gonzalez <mgonzalez4648@gmail.com>
2019-07-01 10:52:45 +0200

    Liberado de las vidas

--- modulos/Vidas.c
+++ modulos/Vidas.c
@@ -1,13 +1,12 @@
 #include "Vidas.h"
 #include "stdlib.h"
 
-struct VidasRep
-{
+struct VidasRep {
     Vida * e;
     int max,n;
 };
-Vidas VidasCrea(int max)
-{
+
+Vidas VidasCrea(int max) {
     Vidas e=malloc(sizeof(struct VidasRep)); // Creación de la estructura
     e->e =malloc(sizeof(Vida)*max);
     e->max=max;
@@ -15,42 +14,36 @@ Vidas VidasCrea(int max)
     return e;
 };
 
-void VidasLibera (Vidas p)
-{
-    for (int i=0; i<p->n; i++)
-    {
+void VidasLibera (Vidas p) {
+    for (int i=0; i<p->n; i++) {
         VidaLibera(p->e[i]);
     }
+
     free(p->e);
     free(p);
 
 };
 
-void VidasDibuja(Vidas p)
-{
+void VidasDibuja(Vidas p) {
     for (int i=0; i<p->n; i++)
         VidaDibuja(p->e[i]);
 
 };
 
-void VidasRellena(Vidas e, Imagen i, int w, int h, int x, int y)
-{
-    while (e->n < e->max)
-    {
+void VidasRellena(Vidas e, Imagen i, int w, int h, int x, int y) {
+    while (e->n < e->max) {
         Vida v1=VidaCrea(i,w,h,x+e->n*30,y);
         e->e[e->n]=v1;
         e->n++;
     }
 }
 
-void VidasEliminaVida(Vidas s)
-{
-    //VidaLibera(s->e[s->n]); FIXME
-    s->n--;
+void VidasEliminaVida(Vidas v) {
+    v->n--;
+    VidaLibera(v->e[v->n]);
 };
 
-int VidasGetVida(Vidas v)
-{
+int VidasGetVida(Vidas v) {
     return (v->n);
 }
 
